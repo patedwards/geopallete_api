353dfa1b78e1e69a07d9cf7cdcb152fbca98438b
patwards <patrick.edwards@biarri.com>
2020-10-23 14:40:25 -0700

    plz

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, current_app
+from datetime import timedelta
+from functools import update_wrapper
 import os
 from numpy import round
 from sklearn.cluster import KMeans
@@ -9,11 +11,61 @@ from PIL import Image
 from io import BytesIO
 from get_map import get_map_by_bbox
 
-from flask_cors import CORS
 app = Flask(__name__)
-cors = CORS(app)
 
+def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
+                attach_to_all=True, automatic_options=True):
+    """Decorator function that allows crossdomain requests.
+      Courtesy of
+      https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
+    """
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    # use str instead of basestring if using Python 3.x
+    if headers is not None and not isinstance(headers, list):
+        headers = ', '.join(x.upper() for x in headers)
+    # use str instead of basestring if using Python 3.x
+    if not isinstance(origin, list):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
 
+    def get_methods():
+        """ Determines which methods are allowed
+        """
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        """The decorator function
+        """
+        def wrapped_function(*args, **kwargs):
+            """Caries out the actual cross domain code
+            """
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 
 def get_image(data, with_features=False):
@@ -84,7 +136,8 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/',  methods=['POST', 'OPTIONS'])
+@crossdomain(origin='*')
 def geopallete():
     print("Method = ", request.method, "!")
     data = json.loads(request.data)
